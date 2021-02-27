b883f1bcd745a580a2e0069a2a2d7d0167d10007
Denis Vasilkovskii <denis.vasilkovskii@tado.com>
2021-02-27 12:53:45 +0100

    Initial Commit

---
+++ Shared/cocktail_db_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  cocktail_db_uiApp.swift
+//  Shared
+//
+//  Created by Denis Vasilkovskii on 27.02.21.
+//
+
+import SwiftUI
+
+@main
+struct cocktail_db_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
